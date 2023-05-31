1c78c5260bf79828e29985074d17bf5c0dac9705
Varzika Tiwari <varzikat@gmail.com>
2023-06-01 00:18:22 +0530

    AllJObs section completed

--- jobposting/src/Components/AllJobs/AllJobs.jsx
+++ jobposting/src/Components/AllJobs/AllJobs.jsx
@@ -2,12 +2,11 @@ import React from "react";
 import Header from "../Headder/Header";
 import axios from "axios";
 import { useEffect, useState } from "react";
-import {CiLocationOn} from "react-icons/ci"
-import './AllJobs.css';
+import { CiLocationOn } from "react-icons/ci";
+import "./AllJobs.css";
 
 function AllJobs() {
-
-const [value, setvalue] = useState([]);
+  const [value, setvalue] = useState([]);
 
   useEffect(() => {
     const user = JSON.parse(sessionStorage.getItem("user"));
@@ -24,7 +23,7 @@ const [value, setvalue] = useState([]);
     axios
       .request(config)
       .then((response) => {
-       setvalue(response.data.data.data);
+        setvalue(response.data.data.data);
         console.log(response.data.data.data);
       })
       .catch((error) => {
@@ -33,24 +32,53 @@ const [value, setvalue] = useState([]);
   }, []);
 
   return (
-    <div>
+    <div className="all-jobs-wrapper">
       {/* <Header/> */}
       <div className="div2-AllJobs">
-        <h3 style={{backgroundColor:"#303F60", color:'white'}}>Jobs posted By you</h3>
+        <h3 style={{ backgroundColor: "#303F60", color: "white" }}>
+          Jobs posted By you
+        </h3>
       </div>
       <div className="textbox-AllJobs">
         {value &&
-          value.map(user => (
-        <div className="textbox" >
-            <span style={{font:"13px", backgroundColor:"#FFFFFF"}}><p style={{backgroundColor:"#FFFFFF"}}>{(user.title)}</p></span>
-            <span className="text">{(user.description)}</span>
-            <div style={ {  display : "flex" , justifyContent : "space-between", marginTop : "40px", backgroundColor:'#FFFFFF'}}>
-      
-            <span className="text"> <CiLocationOn/>{(user.location)}</span>
-            <button style={{backgroundColor:'lightblue', borderRadius:'3px',border:'none', width:'100px', fontSize:'13px'}}>View Applications</button>
+          value.map((user) => (
+            <div className="textbox">
+              <div className="text-container">
+
+              <span style={{ fontSize: "17px", backgroundColor: "#FFFFFF" }}>
+                <p style={{ backgroundColor: "#FFFFFF" }}>{user.title}</p>
+              </span>
+              <span style={{fontSize:'12px'}}>{user.description}</span>
+              </div>
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                  marginTop: "20px",
+                  backgroundColor: "#FFFFFF",
+                }}
+              >
+                <span className="text">
+                  <CiLocationOn
+                    style={{ color: "#43AFFF", height: "30px", width: "30px" }}
+                  />
+                  {user.location}
+                </span>
+                <button
+                  style={{
+                    backgroundColor: "#43AFFF33",
+                    borderRadius: "4px",
+                    border: "none",
+                    minWidth: "100px",
+                    fontSize: "13px",
+                    padding : "3px 10px"
+                  }}
+                >
+                  View Applications
+                </button>
+              </div>
             </div>
-        </div>
-          )) }     
+          ))}
       </div>
       <div className="last-div"></div>
     </div>
